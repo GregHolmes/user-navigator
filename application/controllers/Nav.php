08a107fac901f768ac62996ff950f9a20da09d59
Greg Holmes <greg.holmes@housesimple.com>
2017-10-26 14:50:32 +0100

    Changes style of Nav.php to meet PSR-2 coding style guidelines

--- application/controllers/Nav.php
+++ application/controllers/Nav.php
@@ -1,24 +1,30 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Nav extends CI_Controller {
+class Nav extends CI_Controller
+{
 
 	function __construct()
     {
         parent::__construct();
+
         $this->load->view('session_check');
-        $this->load->model('User_model', '', True);
+        $this->load->model('User_model', '', true);
+
         $username = $this->_getsessionuser();
+
         $data['info'] = $this->User_model->getuserDetails($username);  
         $data['online_users'] = $this->User_model->getOnlineUsers($username);
+
         $this->load->view('raw/basic_header',$data);
         $this->load->view('raw/basic_footer');
     }
     
-     private function _getsessionuser()
+    private function _getsessionuser()
     {
         $p = $this->session->userdata('logged_in');
         $username = $p['username'];
+
         return $username;
     }
 }
