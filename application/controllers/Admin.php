4f7cbe7593b65f191973fa2746622a6493c322b6
Greg Holmes <greg.holmes@housesimple.com>
2017-10-26 14:47:18 +0100

    Changes style of Admin.php to meet PSR-2 coding style guidelines

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,23 +6,30 @@ class Admin extends CI_Controller {
 	function __construct()
     {
         parent::__construct();
+
         $this->load->view('is_admin');
         $this->load->view('admin/admin_header');
         $this->load->view('admin/admin_footer');
         $this->load->model('Admin_model');
     }
-    
-     private function _getsessionuser()
+
+    /**
+     * @return mixed
+     */
+    private function _getsessionuser()
     {
         $p = $this->session->userdata('logged_in');
         $username = $p['username'];
+
         return $username;
     }
+
 
 	public function index()
 	{
 		$this->load->view('admin/index');
 	}
+
     public function viewusers()
     {
         $data['users'] = $this->Admin_model->getAllUsers();
@@ -47,21 +54,24 @@ class Admin extends CI_Controller {
         $this->load->view('admin/viewpaths',$data);
     }
 
-    public function deletepath($p=0)
+    public function deletepath($p = 0)
     {
         $data['lines'] = $this->Admin_model->deletepath($p);
+
         redirect('admin/viewpaths');
     }
 
-    public function deletemarker($p=0)
+    public function deletemarker($p = 0)
     {
         $data['lines'] = $this->Admin_model->deletemarker($p);
+
         redirect('admin/viewmarkers');
     }
 
-    public function deletereview($p=0)
+    public function deletereview($p = 0)
     {
         $data['lines'] = $this->Admin_model->deletereview($p);
+
         redirect('admin/viewreviews');
     }   
 }
