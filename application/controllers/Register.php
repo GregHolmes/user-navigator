98111b07d49bb05bb712af573bd99438ff0e8458
Greg Holmes <greg.holmes@housesimple.com>
2017-10-26 14:51:04 +0100

    Changes style of Register.php to meet PSR-2 coding style guidelines

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -5,22 +5,21 @@ if (!defined('BASEPATH'))
 
 class Register extends CI_Controller
 {
-    
     function __construct()
     {
         parent::__construct();
+
         $this->load->helper('form');
         $this->load->library('form_validation');
         $this->form_validation->set_error_delimiters('', '');
-        $this->load->model('User_model', '', True);
+
+        $this->load->model('User_model', '', true);
         $this->load->helper('security');
     }
     
     public function index()
     {
-        if ($_POST)
-        {
-            
+        if ($_POST) {
             $this->form_validation->set_rules('first_name', 'First name', 'required');
             $this->form_validation->set_rules('last_name', 'Last name', 'required');
             $this->form_validation->set_rules('username', 'Username', 'alpha_numeric|required|is_unique[tbl_users.username]|min_length[5]|max_length[12]|xss_clean');
@@ -30,24 +29,19 @@ class Register extends CI_Controller
             $this->form_validation->set_message('is_natural', 'Please accept TOS');
             $this->form_validation->set_rules('eula', 'TOS', 'is_natural');
             
-            if ($this->form_validation->run() == False)
-            {
+            if ($this->form_validation->run() == false) {
                 echo json_encode(array(
                     'status' => 'error',
                     'message' => validation_errors()
                 ));
-                
-            }
-            else
-            {
+            } else {
                 $first_name = $this->input->post('first_name');
-                $last_name  = $this->input->post('last_name');
-                $full_name  = "$first_name $last_name";
-                $username   = $this->input->post('username');
-                $email      = $this->input->post('email');
-                $password   = $this->input->post('password');
-                
-                
+                $last_name = $this->input->post('last_name');
+                $full_name = "$first_name $last_name";
+                $username = $this->input->post('username');
+                $email = $this->input->post('email');
+                $password = $this->input->post('password');
+
                 $data_array = array(
                     'first_name' => $first_name,
                     'last_name' => $last_name,
@@ -56,16 +50,11 @@ class Register extends CI_Controller
                     'user_email' => $email,
                     'password' => md5($password)
                 );
-                
-                
+
                 $id = $this->User_model->addUser($data_array);
-                
                 $data['id'] = $id;
                 
-                if ($id)
-                {
-                    $sess_array = array();
-                    
+                if ($id) {
                     $sess_array = array(
                         'id' => $id,
                         'username' => $username
@@ -78,51 +67,42 @@ class Register extends CI_Controller
                     ));
                 }
             }
-            
         }
     }
     
     public function is_email_registered()
     {
-        if ($this->input->is_ajax_request())
-        {
+        if ($this->input->is_ajax_request()) {
             $email = $this->input->post('email');
-            if (!$this->form_validation->is_unique($email, 'tbl_users.user_email'))
-            {
+
+            if (!$this->form_validation->is_unique($email, 'tbl_users.user_email')) {
                 $this->output->set_content_type('application/json')->set_output(json_encode(array(
                     'status' => 'error',
                     'message' => 'The email is already taken, choose another one'
                 )));
-            }
-            else
-            {
+            } else {
                 $this->output->set_content_type('application/json')->set_output(json_encode(array(
                     'status' => 'ok'
                 )));
             }
         }
-        
     }
     
     public function checkcr()
     {
-        if ($this->input->is_ajax_request())
-        {
-            $email    = $this->input->post('thisemail');
+        if ($this->input->is_ajax_request()) {
+            $email = $this->input->post('thisemail');
             $username = $this->input->post('thisusername');
             
-            if ($email)
-            {
+            if ($email) {
                 $this->form_validation->set_rules('email', 'Email', 'valid_email|required|is_unique[tbl_users.user_email]');
-                if ($this->form_validation->run() == False)
-                {
+
+                if ($this->form_validation->run() == false) {
                     $this->output->set_content_type('application/json')->set_output(json_encode(array(
                         'status' => 'error',
                         'message' => form_error('email')
                     )));
-                }
-                else
-                {
+                } else {
                     $this->output->set_content_type('application/json')->set_output(json_encode(array(
                         'status' => 'ok',
                         'message' => 'This email is available'
@@ -130,18 +110,15 @@ class Register extends CI_Controller
                 }
             }
             
-            if ($username)
-            {
+            if ($username)  {
                 $this->form_validation->set_rules('username', 'Username', 'alpha_numeric|required|is_unique[tbl_users.username]|min_length[5]|max_length[12]|xss_clean');
-                if ($this->form_validation->run() == False)
-                {
+
+                if ($this->form_validation->run() == false) {
                     $this->output->set_content_type('application/json')->set_output(json_encode(array(
                         'status' => 'error',
                         'message' => form_error('username')
                     )));
-                }
-                else
-                {
+                } else {
                     $this->output->set_content_type('application/json')->set_output(json_encode(array(
                         'status' => 'ok',
                         'message' => 'This username is available',
